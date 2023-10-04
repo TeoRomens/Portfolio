80e0f90accdfe8dc1b09581f95b68b4780299adf
TeoRomens <matteoroman4@gmail.com>
2023-10-04 16:20:49 +0200

    Scroll Indicator

--- app/page.tsx
+++ app/page.tsx
@@ -5,29 +5,34 @@ import {TypeAnimation} from "react-type-animation";
 export default function Home() {
   return (
       <div className="flex min-h-screen items-center justify-center overflow-hidden flex-col bg-[#121212]">
-          <section>
-              <h1 className="text-white mb-4 text-6xl sm:text-5xl lg:text-8xl font-display">
-                  <span className='text-transparent bg-clip-text bg-gradient-to-r from-cyan-700 to-cyan-400'>
-                      Hello, I&apos;m <br/>
-                  </span>
-                  <TypeAnimation
-                      sequence={[
-                          // Same substring at the start will only be typed out once, initially
-                          'Matteo',
-                          1000, // wait 1s before replacing "Mice" with "Hamsters"
-                          'App Developer',
-                          1000,
-                          'Drone Pilot',
-                          1000,
-                          'Basketball Player',
-                          1000
-                      ]}
-                      wrapper="span"
-                      speed={50}
-                      repeat={Infinity}
-                  />
-              </h1>
-          </section>
+          <h1 className="text-white mb-4 text-6xl sm:text-5xl lg:text-8xl font-display">
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-cyan-700 to-cyan-400'>
+                  Hello, I&apos;m <br/>
+              </span>
+              <TypeAnimation
+                  sequence={[
+                      // Same substring at the start will only be typed out once, initially
+                      'Matteo',
+                      1000, // wait 1s before replacing "Mice" with "Hamsters"
+                      'App Developer',
+                      1000,
+                      'Drone Pilot',
+                      1000,
+                      'Basketball Player',
+                      1000
+                  ]}
+                  wrapper="span"
+                  speed={50}
+                  repeat={Infinity}
+              />
+          </h1>
+          <div className="delay-2000 animate-fade-in">
+              <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full absolute bottom-10">
+                  <svg className="w-6 h-6 text-cyan-600" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                      <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                  </svg>
+              </div>
+          </div>
       </div>
   );
 }
